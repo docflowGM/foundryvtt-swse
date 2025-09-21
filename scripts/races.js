66aa7335e05bc54a6c9e4756a088ea1038282b25
docflowGM <149099003+docflowGM@users.noreply.github.com>
2025-09-20 23:33:30 -0400

    Create races.js

---
+++ scripts/races.js
@@ -0,0 +1,8 @@
+export const SWSE_RACES = {
+  human:    { label: "Human",    bonuses: {} },
+  twilek:   { label: "Twi'lek",  bonuses: { cha: 2, con: -2 } },
+  wookiee:  { label: "Wookiee",  bonuses: { str: 2, dex: -2 } },
+  zabrak:   { label: "Zabrak",   bonuses: { con: 2 } },
+  rodian:   { label: "Rodian",   bonuses: { dex: 2, wis: -2 } },
+  custom:   { label: "Custom",   bonuses: {} }
+};
