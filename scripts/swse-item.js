f32d212bdc4c8562095dae607798fd8d9d2e1fee
docflowGM <149099003+docflowGM@users.noreply.github.com>
2025-10-01 17:02:59 -0400

    Update swse-item.js

--- scripts/swse-item.js
+++ scripts/swse-item.js
@@ -1,24 +1,29 @@
 // systems/swse/scripts/swse-item.js
 
-export class SWSEItemSheet extends foundry.appv1?.sheets.foundry?.sheet?.sheets.foundry?.sheet?.sheets.foundry?.sheet?.sheets.ItemSheet {
+/**
+ * SWSE Item Sheet
+ * Custom sheet for SWSE items (weapons, armor, feats, talents, etc.)
+ */
+export class SWSEItemSheet extends ItemSheet {
   /** Configure default options for the SWSE item sheet */
   static get defaultOptions() {
-    return foundry.utils.foundry.utils.foundry.utils.foundry.utils.foundry.utils.mergeObject(super.defaultOptions, {
+    return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["swse", "sheet", "item"],
       template: "systems/swse/templates/item/item-sheet.hbs",
       width: 520,
-      height: "auto", // Can leave as "auto" if you want scroll-flexing
+      height: "auto", // Allows scroll-flexing
       tabs: [
-        { navSelector: "?.sheet-tabs", contentSelector: "?.sheet-body", initial: "data" }
+        { navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "data" }
       ]
     });
   }
 
   /** Provide data to the template */
-  getData(options) {
+  getData(options = {}) {
     const data = super.getData(options);
 
-    // Foundry v10+ already provides `data.system`, but in case older helpers expect it:
+    // Foundry v10+ already provides `data.system`,
+    // but in case older helpers expect it:
     if (!data.system && data.item?.system) {
       data.system = data.item.system;
     }
@@ -26,3 +31,11 @@ export class SWSEItemSheet extends foundry.appv1?.sheets.foundry?.sheet?.sheets.
     return data;
   }
 }
+
+/**
+ * Register SWSE item sheets
+ */
+Hooks.once("init", function() {
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("swse", SWSEItemSheet, { makeDefault: true });
+});
