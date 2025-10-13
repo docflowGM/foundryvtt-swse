61934476625b73baaa09e9375b3d4f89fbc7b218
docflowGM <codytylerwolfe@gmail.com>
2025-10-13 13:26:39 -0400

    clean up clean up everybody everywhere

--- consolidation_backup/20251012_162514/scripts/swse-npc.js
+++
@@ -1,5 +0,0 @@
-// ============================================
-// FILE: scripts/swse-npc.js
-// ============================================
-import { SWSEActorSheet } from "./swse-actor.js";
-... (rest of your JS code from npc file) ...
