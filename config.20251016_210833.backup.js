8fe55950db83aa09101110c405e90215b122d414
docflowGM <codytylerwolfe@gmail.com>
2025-10-16 21:11:39 -0400

    remove backups

--- config.20251016_210833.backup.js
+++
@@ -1,7 +0,0 @@
-// ============================================
-// FILE: config.js
-// ============================================
-export const SWSE = {};
-
-SWSE.actorTypes = ["character", "droid", "vehicle", "npc"];
-SWSE.itemTypes = ["armor", "class", "equipment", "feat", "forcepower", "talent", "weapon", "skill", "attribute", "equipment", "combat-action", "extra-skill-use"];
