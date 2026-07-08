e30a55c21a7de6fa7b14656040660655618401f5
docflowGM <149099003+docflowGM@users.noreply.github.com>
2026-07-08 09:20:17 -0400

    Register armor hydration defense hotfix

--- scripts/patches/runtime-bugfix-hotfixes-init.js
+++ scripts/patches/runtime-bugfix-hotfixes-init.js
@@ -1,5 +1,7 @@
 import { registerRuntimeBugfixHotfixes } from '/systems/foundryvtt-swse/scripts/patches/runtime-bugfix-hotfixes.js';
 import { registerProgressionLedgerReconciliationHotfix } from '/systems/foundryvtt-swse/scripts/patches/progression-ledger-reconciliation-hotfix.js';
+import { registerArmorHydrationDefenseHotfix } from '/systems/foundryvtt-swse/scripts/patches/armor-hydration-defense-hotfix.js';
 
 registerRuntimeBugfixHotfixes();
 registerProgressionLedgerReconciliationHotfix();
+registerArmorHydrationDefenseHotfix();
