cef685a501da82471a55e44496912c56261b33ed
docflowGM <149099003+docflowGM@users.noreply.github.com>
2026-07-08 11:13:22 -0400

    Register attack dialog combat corrections hotfix

--- scripts/patches/runtime-bugfix-hotfixes-init.js
+++ scripts/patches/runtime-bugfix-hotfixes-init.js
@@ -1,7 +1,9 @@
 import { registerRuntimeBugfixHotfixes } from '/systems/foundryvtt-swse/scripts/patches/runtime-bugfix-hotfixes.js';
 import { registerProgressionLedgerReconciliationHotfix } from '/systems/foundryvtt-swse/scripts/patches/progression-ledger-reconciliation-hotfix.js';
 import { registerArmorHydrationDefenseHotfix } from '/systems/foundryvtt-swse/scripts/patches/armor-hydration-defense-hotfix.js';
+import { registerAttackDialogCombatCorrectionsHotfix } from '/systems/foundryvtt-swse/scripts/patches/attack-dialog-combat-corrections-hotfix.js';
 
 registerRuntimeBugfixHotfixes();
 registerProgressionLedgerReconciliationHotfix();
 registerArmorHydrationDefenseHotfix();
+registerAttackDialogCombatCorrectionsHotfix();
