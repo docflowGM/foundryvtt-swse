1785d4243391e0a2460b7a315f446d56e215f28a
docflowGM <149099003+docflowGM@users.noreply.github.com>
2026-07-08 09:05:31 -0400

    Register progression ledger reconciliation hotfix

--- scripts/patches/runtime-bugfix-hotfixes-init.js
+++ scripts/patches/runtime-bugfix-hotfixes-init.js
@@ -1,3 +1,5 @@
 import { registerRuntimeBugfixHotfixes } from '/systems/foundryvtt-swse/scripts/patches/runtime-bugfix-hotfixes.js';
+import { registerProgressionLedgerReconciliationHotfix } from '/systems/foundryvtt-swse/scripts/patches/progression-ledger-reconciliation-hotfix.js';
 
 registerRuntimeBugfixHotfixes();
+registerProgressionLedgerReconciliationHotfix();
