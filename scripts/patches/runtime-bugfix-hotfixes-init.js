9673c805681e439a3f380c7ebf0d2a554ba8028a
docflowGM <149099003+docflowGM@users.noreply.github.com>
2026-07-17 12:12:29 -0400

    Register combat UI behavior hotfixes

--- scripts/patches/runtime-bugfix-hotfixes-init.js
+++ scripts/patches/runtime-bugfix-hotfixes-init.js
@@ -2,8 +2,10 @@ import { registerRuntimeBugfixHotfixes } from '/systems/foundryvtt-swse/scripts/
 import { registerProgressionLedgerReconciliationHotfix } from '/systems/foundryvtt-swse/scripts/patches/progression-ledger-reconciliation-hotfix.js';
 import { registerArmorHydrationDefenseHotfix } from '/systems/foundryvtt-swse/scripts/patches/armor-hydration-defense-hotfix.js';
 import { registerAttackDialogCombatCorrectionsHotfix } from '/systems/foundryvtt-swse/scripts/patches/attack-dialog-combat-corrections-hotfix.js';
+import { registerCombatUiBehaviorHotfixes } from '/systems/foundryvtt-swse/scripts/patches/combat-ui-behavior-hotfix.js';
 
 registerRuntimeBugfixHotfixes();
 registerProgressionLedgerReconciliationHotfix();
 registerArmorHydrationDefenseHotfix();
 registerAttackDialogCombatCorrectionsHotfix();
+registerCombatUiBehaviorHotfixes();
